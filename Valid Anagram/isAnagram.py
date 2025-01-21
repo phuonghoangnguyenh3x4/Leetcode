d727430f72833562a54b1ec25ba48c5d54080688
phuonghoangnguyenh3x4 <52743741+phuonghoangnguyenh3x4@users.noreply.github.com>
2025-01-21 23:26:53 +0700

    Create isAnagram.py

---
+++ Valid Anagram/isAnagram.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        count = defaultdict(int)
+        for c in  s:
+            count[c] += 1
+        for c in t:
+            count[c] -= 1
+        return all([v == 0 for k, v in count.items()])
